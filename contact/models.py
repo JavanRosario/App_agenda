2802ef5ab8173fbced34bca2df9c35200db5c17d
JavanRosario <oliveiraajavan@hotmail.com>
2025-09-21 23:24:33 -0300

    initial SQL modulations

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45, blank=True)
+    phone = models.CharField(max_length=45)
+    email = models.EmailField(max_length=254)
+    crated_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
