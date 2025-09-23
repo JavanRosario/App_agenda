8eb5481c352f981720cbd764df23a99447cd2966
JavanOliveira <oliveiraajavan@hotmail.com>
2025-09-23 17:58:08 -0300

    add __str__ to improve visualization

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id','first_name', 'phone','email','last_name',
+    ordering = 'id',
+    # list_filter = 'created_date',
+    search_fields = 'id','first_name','id',
+    list_per_page = 1
+    list_max_show_all =500
+    list_editable = 'last_name','first_name',
+    list_display_links ='phone',
