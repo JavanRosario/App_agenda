8b3ce313412391a823a186fda391dd6c4bf8b273
JavanOliveira <oliveiraajavan@hotmail.com>
2025-09-27 14:42:21 -0300

    config imgsand static files DIRS

--- contact/urls.py
+++ contact/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import index
-
+from django.conf.urls.static import static
+from django.conf import settings
 app_name = 'contact'
 
 urlpatterns = [
@@ -10,3 +11,7 @@ urlpatterns = [
         name='index'
     )
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_ROOT, document_root=settings.STATIC_ROOT)
