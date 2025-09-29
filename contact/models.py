46357b38fc940ecf858df14bcf5bc6ece8aa4411
JavanRosario <oliveiraajavan@hotmail.com>
2025-09-28 23:17:04 -0300

    add new colum database, category FK and name & id

--- contact/models.py
+++ contact/models.py
@@ -2,6 +2,16 @@ from django.db import models
 from django.utils import timezone
 
 
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=45)
     last_name = models.CharField(max_length=45, blank=True)
@@ -10,7 +20,13 @@ class Contact(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
-    image = models.ImageField(blank=True, upload_to='img/$Y/$m')
+    image = models.ImageField(blank=True, upload_to='img/%Y/%m')
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
 
-    def __str__(self) -> str:   
+    def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
