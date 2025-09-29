2309bfe478db970142ce088ab9019dbbc9336f39
JavanRosario <oliveiraajavan@hotmail.com>
2025-09-28 23:15:40 -0300

    add FK category in django admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
-from .models import Contact
+from .models import Contact, Category
 
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id','first_name', 'phone','email','last_name',
+    list_display = 'id', 'first_name', 'phone', 'email', 'last_name',
     ordering = 'id',
-    # list_filter = 'created_date',
-    search_fields = 'id','first_name','id',
+    search_fields = 'id', 'first_name', 'id',
     list_per_page = 1
-    list_max_show_all =500
-    list_editable = 'last_name','first_name',
-    list_display_links ='phone',
+    list_max_show_all = 500
+    list_editable = 'last_name', 'first_name',
+    list_display_links = 'phone',
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
