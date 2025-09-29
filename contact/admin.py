c25900962f429734238bec6e0aa82554c4a974a5
JavanOliveira <oliveiraajavan@hotmail.com>
2025-09-29 14:40:10 -0300

    change in list_per_page to 100

--- contact/admin.py
+++ contact/admin.py
@@ -7,7 +7,7 @@ class ContactAdmin(admin.ModelAdmin):
     list_display = 'id', 'first_name', 'phone', 'email', 'last_name',
     ordering = 'id',
     search_fields = 'id', 'first_name', 'id',
-    list_per_page = 1
+    list_per_page = 100
     list_max_show_all = 500
     list_editable = 'last_name', 'first_name',
     list_display_links = 'phone',
