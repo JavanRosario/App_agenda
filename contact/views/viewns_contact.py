c0b3de25c6d6f49722f08ee4a0c0eafb78133c76
JavanOliveira <oliveiraajavan@hotmail.com>
2025-10-01 18:33:28 -0300

    add new view SEARCH

--- contact/views/viewns_contact.py
+++ contact/views/viewns_contact.py
@@ -1,7 +1,8 @@
 # type: ignore
 from django.shortcuts import render
 from contact.models import Contact
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
+from django.db.models import Q
 
 
 def index(request):
@@ -27,5 +28,23 @@ def single_contact(request, contact_id):
     return render(
         request,
         'contact/single_contact.html',
+        context,
+    )
+
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+    contacts = Contact.objects.all().filter(show=True).filter(Q(first_name__icontains=search_value) | Q(last_name__icontains=search_value)
+                                                              | Q(phone_name__icontains=search_value) | Q(email_name__icontains=search_value)).order_by('-id')
+    context = {
+        'contacts': contacts,
+        'main_title': 'Contatos - ',
+    }
+    return render(
+        request,
+        'contact/index.html',
         context,
     )
