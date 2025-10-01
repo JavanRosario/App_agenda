35ee8e6c8f1c16bd5d75166148beecbb3a994cdf
JavanRosario <oliveiraajavan@hotmail.com>
2025-10-01 08:34:27 -0300

    add new view single contact

--- contact/views/viewns_contact.py
+++ contact/views/viewns_contact.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from contact.models import Contact
+from django.http import Http404
+from django.shortcuts import get_object_or_404
 
 
 def index(request):
@@ -10,5 +12,18 @@ def index(request):
     return render(
         request,
         'contact/index.html',
+        context,
+    )
+
+
+def single_contact(request, contact_id):
+    single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    context = {
+        'contact': single_contact,
+    }
+
+    return render(
+        request,
+        'contact/single_contact.html',
         context,
     )
