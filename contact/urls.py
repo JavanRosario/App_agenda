f44d5e5175e817e0d3d7bde12bfc6951548e414f
JavanRosario <oliveiraajavan@hotmail.com>
2025-10-01 08:35:01 -0300

    add new url pattern

--- contact/urls.py
+++ contact/urls.py
@@ -1,15 +1,20 @@
 from django.urls import path
-from .views import index
+from .views import index, single_contact
 from django.conf.urls.static import static
 from django.conf import settings
 app_name = 'contact'
 
 urlpatterns = [
     path(
+        '<int:contact_id>/',
+        single_contact,
+        name='contact'
+    ),
+    path(
         '',
         index,
         name='index'
-    )
+    ),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
