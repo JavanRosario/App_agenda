116ffe278f80b9a2d1d39826917769d04c36ac42
JavanOliveira <oliveiraajavan@hotmail.com>
2025-10-02 17:01:21 -0300

    add create view url

--- contact/urls.py
+++ contact/urls.py
@@ -1,24 +1,16 @@
 from django.urls import path
-from .views import index, single_contact, search
+from .views import index, single_contact, search,create
 from django.conf.urls.static import static
 from django.conf import settings
 app_name = 'contact'
 
 urlpatterns = [
-    path(
-        '<int:contact_id>/',
-        single_contact,
-        name='contact'
-    ),
-    path(
-        '',
-        index,
-        name='index'
-    ), path(
-        'search/',
-        search,
-        name='search'
-    ),
+    path('', index, name='index'),
+    path('search/', search, name='search'),
+    path('contact/<int:contact_id>/detail/', single_contact, name='contact'),
+
+    path('contact/create/', create, name='create'),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
