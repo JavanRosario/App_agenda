7e1a47de65920cd879763fa3000d27366a28af83
JavanRosario <oliveiraajavan@hotmail.com>
2025-10-01 23:34:22 -0300

    fix view error

--- contact/views/viewns_contact.py
+++ contact/views/viewns_contact.py
@@ -33,12 +33,20 @@ def single_contact(request, contact_id):
 
 
 def search(request):
-    search_value = request.GET.get('q', '').strip()
+    search_value = request.GET.get('q', '').strip().title()
 
     if search_value == '':
         return redirect('contact:index')
-    contacts = Contact.objects.all().filter(show=True).filter(Q(first_name__icontains=search_value) | Q(last_name__icontains=search_value)
-                                                              | Q(phone_name__icontains=search_value) | Q(email_name__icontains=search_value)).order_by('-id')
+
+    contacts = Contact.objects.all().filter(show=True).filter(
+        Q(first_name__icontains=search_value) |
+        Q(last_name__icontains=search_value) |
+        Q(phone__icontains=search_value) |
+        Q(email__icontains=search_value)
+    ).order_by('-id')
+
+    print(contacts.query)
+
     context = {
         'contacts': contacts,
         'main_title': 'Contatos - ',
