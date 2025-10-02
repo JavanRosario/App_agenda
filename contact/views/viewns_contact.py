45d5dcce34e276c641308c9b9707093e7c462566
JavanOliveira <oliveiraajavan@hotmail.com>
2025-10-02 17:00:43 -0300

    add pagination objects in views

--- contact/views/viewns_contact.py
+++ contact/views/viewns_contact.py
@@ -3,12 +3,18 @@ from django.shortcuts import render
 from contact.models import Contact
 from django.shortcuts import get_object_or_404, redirect
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 
 def index(request):
     contacts = Contact.objects.all().filter(show=True).order_by('-id')
+
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'main_title': 'Contatos - ',
     }
     return render(
@@ -20,6 +26,7 @@ def index(request):
 
 def single_contact(request, contact_id):
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
+
     context = {
         'contact': single_contact,
         'single_title': f'{single_contact.first_name} {single_contact.last_name} - '
@@ -45,10 +52,14 @@ def search(request):
         Q(email__icontains=search_value)
     ).order_by('-id')
 
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     print(contacts.query)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'main_title': 'Contatos - ',
     }
     return render(
