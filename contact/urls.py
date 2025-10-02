d0699a8515bf0e684c7299fffcfa5e623d67ce64
JavanRosario <oliveiraajavan@hotmail.com>
2025-10-01 23:33:59 -0300

    fix url error

--- contact/urls.py
+++ contact/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, single_contact
+from .views import index, single_contact, search
 from django.conf.urls.static import static
 from django.conf import settings
 app_name = 'contact'
@@ -14,9 +14,9 @@ urlpatterns = [
         '',
         index,
         name='index'
-    ),path(
+    ), path(
         'search/',
-        index,
+        search,
         name='search'
     ),
 ]
