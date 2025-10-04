b411bd369786a17a4928e0e72fccab65fc90db73
JavanOliveira <oliveiraajavan@hotmail.com>
2025-10-04 14:00:21 -0300

    initial validating fields

--- contact/forms.py
+++ contact/forms.py
@@ -1,17 +1,40 @@
+# type: ignore
 from django import forms
 from django.core.exceptions import ValidationError
 from .models import Contact
-from typing import Any
+
 
 class Form(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['first_name'].widget.attrs.update({
+            'class': 'class_a class_b',
+            'placeholder': 'primeiro nome'
+        })
+        self.fields['first_name'].help_text = 'Coloque seu nome completo'
+
     class Meta:
         model = Contact
         fields = ('first_name', 'last_name', 'phone',)
 
-    def clean(self) -> dict[str, Any]:
+    def clean(self):
+        error_msg = ValidationError(
+            'Primeiro nome não pode ser igual ao sobrenome', code='invalid')
         cleaned_data = self.cleaned_data
-        self.add_error('first_name', ValidationError(
-            'Erro no nome', code='invalid'))
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+
+        if first_name == last_name:
+            self.add_error('first_name', error_msg)
+            self.add_error('last_name', error_msg)
         return super().clean()
 
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
 
+        if '#$%&*()_+!@' in first_name:
+            self.add_error('first_name', ValidationError(
+                'Sem caracteres expeciais', code='invalide'))
+
+        return first_name
