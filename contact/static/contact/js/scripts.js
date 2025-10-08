357eb90700999af2d3123cbf4eea88b8b46c837f
JavanOliveira <oliveiraajavan@hotmail.com>
2025-10-08 17:20:36 -0300

    add toLocaleDateString function

--- contact/static/contact/js/scripts.js
+++ contact/static/contact/js/scripts.js
@@ -1,10 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const alert = document.querySelectorAll(".custom-alert");
-  alert.forEach((alert) => {
+  const alerts = document.querySelectorAll(".custom-alert");
+  alerts.forEach((alert) => {
     setTimeout(() => {
       alert.style.opacity = "0";
       setTimeout(() => alert.remove(), 500);
     }, 3000);
   });
+  
+  const html_ = document.querySelector(".data p");
+  if (html_) {
+    const data = new Date();
+    html_.innerHTML = data.toLocaleDateString("pt-BR", {
+      dateStyle: "full",
+    });
+  }
 });
-    
